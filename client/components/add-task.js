95ca586890f19b68827c8222cac9ce040dc764ba
Alex Parshin <aip@l351lion.OTDEL35.local>
2020-09-02 16:43:17 +0300

    fixed error of output of the edited file

--- client/components/add-task.js
+++ client/components/add-task.js
@@ -1,24 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 
 const AddTask = (props) => {
-  const [value, setValue] = useState('')
+  // const [value, setValue] = useState('')
 
   async function handleClick () {
-    await props.setNewTask(value)
-    await setValue('')
+    await props.setNewTask(props.inputTaskTitle)
+    await props.setInputTaskTitle('')
   }
 
   const onChange = (e) => {
-    if (props.inputTaskTitle !== '') {
-      setValue(props.inputTaskTitle)
-      props.setInputTaskTitle('')
-    } else{
-      setValue(e.target.value)
-      props.setNewEditTask(e.target.value)
-
-    }
-    console.log('add-task.inputValue: ', value)
+    props.setInputTaskTitle(e.target.value)
+    props.setNewEditTask(e.target.value)
+    console.log('add-task.inputValue: ', props.inputTaskTitle)
   }
 
 
@@ -27,7 +21,7 @@ const AddTask = (props) => {
       <form className="w-full py-2 px-4">
         <div className="flex items-center border-b border-b-2 border-blue-500 py-2">
           <input 
-            value={value}
+            value={props.inputTaskTitle}
             className="appearance-none bg-transparent border-none w-full mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             placeholder="New task"
